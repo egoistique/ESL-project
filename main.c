73e37d1241a3888d49153514036640b8f5cbdc6f
master <katyamit5@gmail.com>
2023-11-12 19:03:50 +0000

    new makefile and sdk_config.h
    References #3

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include "nrf_gpio.h"
 #include "nrfx_systick.h"
 #include "nrfx_gpiote.h"
+#include "app_timer.h"
+
 
 #define LED1_PIN NRF_GPIO_PIN_MAP(0, 6)
 #define LED2_PIN NRF_GPIO_PIN_MAP(0, 8)
@@ -21,12 +23,17 @@
 #define BLINK_PERIOD_US (1000000/BLINK_FREQ) 
 
 #define PERIOD 1000
+#define DEBOUNCE_TIME_MS 10
 
-static const unsigned int device_id[] = {6, 5, 8, 1};
+static const unsigned int device_id[] = {2, 5, 8, 1};
 static const int32_t leds_list[] = CUSTOM_LEDS_LIST;
 
 
 static volatile bool blink_enable = true;
+
+static volatile bool button_state = true;  // Используйте pull-up, поэтому начальное состояние true
+static volatile int32_t last_button_press_time = 0;
+
 
 void blink(int led){
     bsp_board_led_invert(led);
@@ -116,13 +123,17 @@ void smooth_blink(int32_t led, int32_t duration_ms, volatile bool * enable)
 
 }
 
-void smooth_blink_any_times(int32_t led , int32_t num, int32_t half_period_ms, volatile bool *enable)
+void smooth_blink_any_times(int32_t led, int32_t num, int32_t half_period_ms, volatile bool *enable)
 {
     for(unsigned int i = 0; i < num; i++){
-        smooth_blink(led, half_period_ms, enable);
-        nrf_delay_ms(half_period_ms);
+        while (*enable) {
+            smooth_blink(led, half_period_ms, enable);
+            nrf_delay_ms(half_period_ms);
+        }
+        nrf_delay_ms(half_period_ms);  // Дополнительная задержка после заморозки
     }
 }
+
 
 void all_leds_off(unsigned int num, const int32_t leds[num])
 {
@@ -130,6 +141,29 @@ void all_leds_off(unsigned int num, const int32_t leds[num])
         led_off(leds[i]);
     }
 }
+
+
+void check_double_click(void)
+{
+    if (!button_pressed(BUTTON_PIN)) {
+        // Кнопка не нажата
+        return;
+    }
+
+    int32_t current_time = app_timer_cnt_get();
+
+    int32_t time_since_last_press = current_time - last_button_press_time;
+
+    if (button_state && (time_since_last_press > DEBOUNCE_TIME_MS) && (time_since_last_press < (2 * DEBOUNCE_TIME_MS))) {
+        // Обработка двойного нажатия
+        blink_enable = !blink_enable;  // Инвертируем состояние
+        while (!button_pressed(BUTTON_PIN));  // Ждем, пока кнопка не отпущена
+    }
+
+    button_state = button_pressed(BUTTON_PIN);
+    last_button_press_time = current_time;
+}
+
 
 int main(void)
 {
@@ -141,6 +175,9 @@ int main(void)
     nrfx_systick_init();
 
     while(true) {
+
+        check_double_click();
+
         for (int i = 0; i < sizeof(device_id)/sizeof(*device_id); i++)
         {
             smooth_blink_any_times(leds_list[i], device_id[i], PERIOD, &blink_enable);
