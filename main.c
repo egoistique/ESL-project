4a8fb72a1650f201ee0e9e03812ff78393561a2d
master <katyamit5@gmail.com>
2023-11-20 17:55:03 +0000

    It makes sense, but double-clicking doesn't work((((((((((
    References #3

--- main.c
+++ main.c
@@ -11,38 +11,56 @@
 #include "my_blink.h"
 
 #define DEBOUNCE_TIME_MS 10
-#define DOUBLE_CLICK_TIME_MS 500
+#define DOUBLE_CLICK_TIME_MS 2000
 
 static const unsigned int device_id[] = {1, 5, 8, 1};
 static const int32_t leds[] = LEDS;
 static volatile bool blink_enable = true;
 static volatile bool button_clicked = false;
+static volatile unsigned int click_num = 0;
+static volatile bool debounce_in_progress = false;
+
+static app_timer_id_t debounce_timer;
 static app_timer_id_t double_click_timer;
 
 
 void double_click_timeout_handler(void* p_context)
 {
-    button_clicked = false;
+    if (click_num == 2) {
+        blink_enable = !blink_enable;
+        click_num = 0;
+    }
 }
 
-void button_handler(nrfx_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
+
+void debounce_timer_handler(void* p_context)
+{
+    debounce_in_progress = false;
+}
+
+
+void debounce_handler(nrfx_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
 {
     if (action == NRF_GPIOTE_POLARITY_HITOLO)
     {
-        if (!button_clicked)
+        if (debounce_in_progress)
         {
-            button_clicked = true;
-            ret_code_t err_code = app_timer_start(double_click_timer, APP_TIMER_TICKS(DOUBLE_CLICK_TIME_MS), NULL);
+            ret_code_t err_code = app_timer_start(debounce_timer, APP_TIMER_TICKS(DEBOUNCE_TIME_MS), NULL);
             APP_ERROR_CHECK(err_code);
         }
         else
         {
-            blink_enable = !blink_enable; 
-            app_timer_stop(double_click_timer); 
-            button_clicked = false; 
+            click_num++;
+            debounce_in_progress = true;
+            ret_code_t err_code = app_timer_start(debounce_timer, APP_TIMER_TICKS(DEBOUNCE_TIME_MS), NULL);
+            err_code = app_timer_start(double_click_timer, APP_TIMER_TICKS(DOUBLE_CLICK_TIME_MS), NULL);
+            APP_ERROR_CHECK(err_code);
+
         }
+
     }
 }
+
 
 int main(void)
 {
@@ -53,12 +71,18 @@ int main(void)
     nrfx_systick_init();
     nrfx_gpiote_init();
 
-    ret_code_t err_code = app_timer_create(&double_click_timer, APP_TIMER_MODE_SINGLE_SHOT, double_click_timeout_handler);
+
+    ret_code_t err_code = app_timer_create(&debounce_timer, APP_TIMER_MODE_SINGLE_SHOT, debounce_timer_handler);
     APP_ERROR_CHECK(err_code);
 
+    err_code = app_timer_create(&double_click_timer, APP_TIMER_MODE_SINGLE_SHOT, double_click_timeout_handler);
+    APP_ERROR_CHECK(err_code);
+
+
     nrfx_gpiote_in_config_t button_config = NRFX_GPIOTE_CONFIG_IN_SENSE_HITOLO(true);
-    nrfx_gpiote_in_init(BUTTON_PIN, &button_config, button_handler);
+    nrfx_gpiote_in_init(BUTTON_PIN, &button_config, debounce_handler);
     nrfx_gpiote_in_event_enable(BUTTON_PIN, true);
+
 
     while(true) {
         for (int i = 0; i < sizeof(device_id)/sizeof(*device_id); i++)
