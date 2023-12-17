05671f3592579e60ad746e883fcd278e687fe6c8
master <katyamit5@gmail.com>
2023-12-17 14:27:10 +0000

    trying to attach an example, the board is recognized as a flash drive but dev/ttyACM1 is not found
    References #6

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
-#include <stdbool.h>
 #include <stdint.h>
+
+#include "sdk_config.h"
+#include "nrfx_pwm.h"
+
 #include "my_leds.h"
 #include "my_button.h"
 #include "my_blink.h"
@@ -12,19 +15,92 @@
 #include "nrfx_nvmc.h"
 #include "nvmc.h"
 
-#include "nordic_common.h"
-#include "boards.h"
-
-#include "nrf_log.h"
-#include "nrf_log_ctrl.h"
-#include "nrf_log_default_backends.h"
-
-#include "nrf_log_backend_usb.h"
-
 #include "app_usbd.h"
 #include "app_usbd_serial_num.h"
+#include "app_usbd_cdc_acm.h"
 
-static const int32_t leds[] = LEDS;
+
+#define READ_SIZE 1
+
+static char m_rx_buffer[READ_SIZE];
+
+static void usb_ev_handler(app_usbd_class_inst_t const * p_inst,
+                           app_usbd_cdc_acm_user_event_t event);
+
+/* Make sure that they don't intersect with LOG_BACKEND_USB_CDC_ACM */
+#define CDC_ACM_COMM_INTERFACE  2
+#define CDC_ACM_COMM_EPIN       NRF_DRV_USBD_EPIN3
+
+#define CDC_ACM_DATA_INTERFACE  3
+#define CDC_ACM_DATA_EPIN       NRF_DRV_USBD_EPIN4
+#define CDC_ACM_DATA_EPOUT      NRF_DRV_USBD_EPOUT4
+
+APP_USBD_CDC_ACM_GLOBAL_DEF(usb_cdc_acm,
+                            usb_ev_handler,
+                            CDC_ACM_COMM_INTERFACE,
+                            CDC_ACM_DATA_INTERFACE,
+                            CDC_ACM_COMM_EPIN,
+                            CDC_ACM_DATA_EPIN,
+                            CDC_ACM_DATA_EPOUT,
+                            APP_USBD_CDC_COMM_PROTOCOL_AT_V250);
+
+
+
+static void usb_ev_handler(app_usbd_class_inst_t const * p_inst,
+                           app_usbd_cdc_acm_user_event_t event)
+{
+    switch (event) {
+    case APP_USBD_CDC_ACM_USER_EVT_PORT_OPEN: {
+        ret_code_t ret;
+        // bsp_board_led_on(0);
+        ret = app_usbd_cdc_acm_read(&usb_cdc_acm, m_rx_buffer, READ_SIZE);
+        UNUSED_VARIABLE(ret);
+        break;
+    }
+    case APP_USBD_CDC_ACM_USER_EVT_PORT_CLOSE: {
+        // bsp_board_led_off(0);
+        break;
+    }
+    case APP_USBD_CDC_ACM_USER_EVT_TX_DONE: {
+        NRF_LOG_INFO("tx done");
+        // bsp_board_led_invert(1);
+        break;
+    }
+    case APP_USBD_CDC_ACM_USER_EVT_RX_DONE: {
+        // bsp_board_led_invert(2);
+        ret_code_t ret;
+        do {
+            /*Get amount of data transfered*/
+            size_t size = app_usbd_cdc_acm_rx_size(&usb_cdc_acm);
+            NRF_LOG_INFO("rx size: %d", size);
+
+            /* It's the simple version of an echo. Note that writing doesn't
+             * block execution, and if we have a lot of characters to read and
+             * write, some characters can be missed.
+             */
+            if (m_rx_buffer[0] == '\r' || m_rx_buffer[0] == '\n') {
+                ret = app_usbd_cdc_acm_write(&usb_cdc_acm, "\r\n", 2);
+            }
+            else {
+                ret = app_usbd_cdc_acm_write(&usb_cdc_acm,
+                                             m_rx_buffer,
+                                             READ_SIZE);
+            }
+
+            /* Fetch data until internal buffer is empty */
+            ret = app_usbd_cdc_acm_read(&usb_cdc_acm,
+                                        m_rx_buffer,
+                                        READ_SIZE);
+        } while (ret == NRF_SUCCESS);
+
+        break;
+    }
+    default:
+        break;
+    }
+}
+
+ static const int32_t leds[] = LEDS;
 
 void logs_init()
 {
@@ -34,17 +110,20 @@ void logs_init()
     NRF_LOG_DEFAULT_BACKENDS_INIT();
 }
 
+
 int main(void)
 {
     logs_init();
     configure_leds(sizeof(leds) / sizeof(*leds), leds);
     configure_button(BUTTON_PIN);
-    lfclk_request();
+    //lfclk_request();
     timers_init();
 
-    NRF_LOG_INFO("Starting up the test project with USB logging");
+    app_usbd_class_inst_t const * class_cdc_acm = app_usbd_cdc_acm_class_inst_get(&usb_cdc_acm);
+    ret_code_t ret = app_usbd_class_append(class_cdc_acm);
+    APP_ERROR_CHECK(ret);
 
-    struct RGB the_color;
+     struct RGB the_color;
 
     read_data_from_nvm(&hsv_color);
 
@@ -56,9 +135,13 @@ int main(void)
     nrfx_pwm_init(&pwm_inst, &pwm_config, pwm_handler);
     nrfx_pwm_simple_playback(&pwm_inst, &pwm_sequence, PWM_PLAYBACK_COUNT, NRFX_PWM_FLAG_LOOP);
 
-    while (true)
-    {
+
+    while(true) {
+        // while (app_usbd_event_queue_process()) {
+        //     /* Nothing to do */
+        // }
+
         LOG_BACKEND_USB_PROCESS();
         NRF_LOG_PROCESS();
-    }
+    }   
 }
---
+++ pca10059/armgcc/_build/nrf52840_xxaa/states.c.d
@@ -0,0 +1,9 @@
+_build/nrf52840_xxaa/states.c.o: ../../src/states.c \
+ ../../headers/states.h /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h
+
+../../headers/states.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h:
