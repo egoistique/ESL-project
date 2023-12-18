2acfbd295a5c2c411a45a1a4d729224517c9da49
master <katyamit5@gmail.com>
2023-12-18 12:15:15 +0000

    Remove _build directory from tracking

--- pca10059/armgcc/_build/nrf52840_xxaa/states.c.d
+++
@@ -1,9 +0,0 @@
-_build/nrf52840_xxaa/states.c.o: ../../src/states.c \
- ../../headers/states.h /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
- /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h
-
-../../headers/states.h:
-
-/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
-
-/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h:
